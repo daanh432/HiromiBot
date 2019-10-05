c900f2c6462810033c273669f2424d89595451a1
daanh432 <daan@daanhendriks.nl>
2019-10-05 23:43:55 +0200

    Found out what the manifest file is for and fixed it.

--- src/main/java/nl/daanh/hiromibot/Secrets.java
+++
@@ -1,8 +0,0 @@
-package nl.daanh.hiromibot;
-
-public class Secrets {
-    public static final String PREFIX = "hi!";
-    public static final long OWNER = 174509580196249600L;
-    static final String TOKEN = "NjI5MDQ0NTgzMjU4MzkwNTM4.XZYwAA.QvUYXnOSy28Y-C2Gq0zN4nDJsmI";
-
-}
