7a7077b7cda3e56610eab3f6d2b4293c4ca23294
daanh432 <daan@daanhendriks.nl>
2020-06-08 13:50:22 +0200

    Added the SQLite Data Source

--- src/main/java/nl/daanh/hiromibot/database/SQLiteDataSource.java
+++ src/main/java/nl/daanh/hiromibot/database/SQLiteDataSource.java
@@ -10,10 +10,8 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Nullable;
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SQLiteDataSource implements DatabaseManager {
@@ -42,15 +40,15 @@ public class SQLiteDataSource implements DatabaseManager {
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
         ds = new HikariDataSource(config);
+        ds.setConnectionTimeout(3000L);
 
         try (final Statement statement = getConnection().createStatement()) {
-            final String defaultPrefix = Config.getInstance().getString("prefix");
-
             // language=SQLite
             statement.execute("CREATE TABLE IF NOT EXISTS guild_settings (" +
                     "id INTEGER PRIMARY KEY AUTOINCREMENT," +
                     "guild_id VARCHAR(20) NOT NULL," +
-                    "prefix VARCHAR(255) NOT NULL DEFAULT '" + defaultPrefix + "'" +
+                    "setting VARCHAR(20) NOT NULL," +
+                    "value VARCHAR(255) NOT NULL" +
                     ");");
 
             LOGGER.info("Table initialised");
@@ -63,16 +61,37 @@ public class SQLiteDataSource implements DatabaseManager {
         return ds.getConnection();
     }
 
+    private boolean settingExists(long guildId, String setting) {
+        try {
+            // language=SQLite
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = ?");
+            statement.setString(1, String.valueOf(guildId));
+            statement.setString(2, setting);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return false;
+    }
+
     @Override
     public String getPrefix(long guildId) {
         try {
             // language=SQLite
-            PreparedStatement statement = this.getConnection().prepareStatement("SELECT prefix FROM guild_settings WHERE guild_id = ?");
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = ?");
             statement.setString(1, String.valueOf(guildId));
+            statement.setString(2, "prefix");
 
-            statement.execute();
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getString("value");
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage());
         }
 
         return Config.getInstance().getString("prefix");
@@ -81,59 +100,192 @@ public class SQLiteDataSource implements DatabaseManager {
     @Override
     public void setPrefix(long guildId, String newPrefix) {
         try {
-            // language=SQLite
-            PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET prefix = ? WHERE guild_id = ?");
-            statement.setString(1, newPrefix);
-            statement.setString(2, String.valueOf(guildId));
-            statement.executeUpdate();
+            if (this.settingExists(guildId, "prefix")) {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET value = ? WHERE guild_id = ? AND setting = 'prefix'");
+                statement.setString(1, newPrefix);
+                statement.setString(2, String.valueOf(guildId));
+                statement.executeUpdate();
+            } else {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO guild_settings (guild_id, setting, value) VALUES (?, 'prefix', ?)");
+                statement.setString(1, String.valueOf(guildId));
+                statement.setString(2, newPrefix);
+                statement.execute();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage());
         }
     }
 
     @Override
     public boolean getMusicEnabled(long guildId) {
-        return false;
+        try {
+            // language=SQLite
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = 'musicEnabled'");
+            statement.setString(1, String.valueOf(guildId));
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getString("value").equalsIgnoreCase("true");
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        return true;
     }
 
     @Override
     public void setMusicEnabled(long guildId, boolean enabled) {
-
+        try {
+            if (this.settingExists(guildId, "musicEnabled")) {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET value = ? WHERE guild_id = ? AND setting = 'musicEnabled'");
+                statement.setString(1, enabled ? "true" : "false");
+                statement.setString(2, String.valueOf(guildId));
+                statement.executeUpdate();
+            } else {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO guild_settings (guild_id, setting, value) VALUES (?, 'musicEnabled', ?)");
+                statement.setString(1, String.valueOf(guildId));
+                statement.setString(2, enabled ? "true" : "false");
+                statement.execute();
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 
     @Override
     public boolean getFunEnabled(long guildId) {
-        return false;
+        try {
+            // language=SQLite
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = 'funEnabled'");
+            statement.setString(1, String.valueOf(guildId));
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getString("value").equalsIgnoreCase("true");
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        return true;
     }
 
     @Override
     public void setFunEnabled(long guildId, boolean enabled) {
-
+        try {
+            if (this.settingExists(guildId, "funEnabled")) {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET value = ? WHERE guild_id = ? AND setting = 'funEnabled'");
+                statement.setString(1, enabled ? "true" : "false");
+                statement.setString(2, String.valueOf(guildId));
+                statement.executeUpdate();
+            } else {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO guild_settings (guild_id, setting, value) VALUES (?, 'funEnabled', ?)");
+                statement.setString(1, String.valueOf(guildId));
+                statement.setString(2, enabled ? "true" : "false");
+                statement.execute();
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 
     @Override
     public boolean getModerationEnabled(long guildId) {
-        return false;
+        try {
+            // language=SQLite
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = 'moderationEnabled'");
+            statement.setString(1, String.valueOf(guildId));
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return resultSet.getString("value").equalsIgnoreCase("true");
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+
+        return true;
     }
 
     @Override
     public void setModerationEnabled(long guildId, boolean enabled) {
-
+        try {
+            if (this.settingExists(guildId, "moderationEnabled")) {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET value = ? WHERE guild_id = ? AND setting = 'moderationEnabled'");
+                statement.setString(1, enabled ? "true" : "false");
+                statement.setString(2, String.valueOf(guildId));
+                statement.executeUpdate();
+            } else {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO guild_settings (guild_id, setting, value) VALUES (?, 'moderationEnabled', ?)");
+                statement.setString(1, String.valueOf(guildId));
+                statement.setString(2, enabled ? "true" : "false");
+                statement.execute();
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 
     @Override
     public List<CommandInterface.CATEGORY> getEnabledCategories(long guildId) {
-        return null;
+        List<CommandInterface.CATEGORY> list = new ArrayList<>();
+        list.add(CommandInterface.CATEGORY.OTHER);
+
+        if (this.getMusicEnabled(guildId)) list.add(CommandInterface.CATEGORY.MUSIC);
+        if (this.getFunEnabled(guildId)) list.add(CommandInterface.CATEGORY.FUN);
+        if (this.getModerationEnabled(guildId)) list.add(CommandInterface.CATEGORY.MODERATION);
+
+        return list;
     }
 
     @Nullable
     @Override
     public Long getCreateVoiceChannelId(long guildId) {
+        try {
+            // language=SQLite
+            PreparedStatement statement = this.getConnection().prepareStatement("SELECT value FROM guild_settings WHERE guild_id = ? AND setting = 'createVoiceChannel'");
+            statement.setString(1, String.valueOf(guildId));
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                return Long.parseLong(resultSet.getString("value"));
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        } catch (NumberFormatException e) {
+            // Ignore user error
+        }
+
         return null;
     }
 
     @Override
     public void setCreateVoiceChannelId(long guildId, long voiceChannelId) {
-
+        try {
+            if (this.settingExists(guildId, "prefix")) {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("UPDATE guild_settings SET value = ? WHERE guild_id = ? AND setting = 'createVoiceChannel'");
+                statement.setString(1, String.valueOf(voiceChannelId));
+                statement.setString(2, String.valueOf(guildId));
+                statement.executeUpdate();
+            } else {
+                // language=SQLite
+                PreparedStatement statement = this.getConnection().prepareStatement("INSERT INTO guild_settings (guild_id, setting, value) VALUES (?, 'createVoiceChannel', ?)");
+                statement.setString(1, String.valueOf(guildId));
+                statement.setString(2, String.valueOf(voiceChannelId));
+                statement.execute();
+            }
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 }
